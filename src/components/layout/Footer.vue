c826df9555516569b54af257119e6606f39cb785
Janet Cushing <jcushing@iapp.org>
2020-05-29 07:43:57 -0400

    br-01 move weather app to footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="footer">
-    <p>&copy;Lovewell Pond Association - All rights reserved.</p>
-    <p>Property Owners of Lovewell Pond Association</p>
-    <p>P.O. Box 617, Fryeburg, ME  04037</p>
-    <p><a href="mailto:lovewellpondassoc@gmail.com" target="_blank">Contact Us</a></p>
-    <br />
-    <p>Website designed by Dawn Sirois, Portsmouth, NH</p>
+    <b-row>
+      <b-col md="2"></b-col>
+      <b-col md="8">
+        <p>&copy;Lovewell Pond Association - All rights reserved.</p>
+        <p>Property Owners of Lovewell Pond Association</p>
+        <p>P.O. Box 617, Fryeburg, ME 04037</p>
+        <p>
+          <a href="mailto:lovewellpondassoc@gmail.com" target="_blank">Contact Us</a>
+        </p>
+        <br />
+        <p>Website designed by Dawn Sirois, Portsmouth, NH</p>
+      </b-col>
+      <b-col md="1">
+            <p >
+                <a href= "https://www.wunderground.com/?cm_ven=cgi" target = "_blank"><img  style="border-style: solid;" src="http://weathersticker.wunderground.com/weathersticker/infobox_both/language/www/US/ME/Fryeburg.gif" 
+                alt="Click for Fryeburg, Maine Forecast" height="108" width="144" ></a>
+                </p>
+        </b-col>
+        <b-col md="1"></b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
-}
+  name: "Footer"
+};
 </script>
 
 <style scoped>
@@ -22,12 +36,11 @@ export default {
   text-align: center;
   padding: 10px;
   font: 14px/10px Ubuntu;
-    font-family: 'Ubuntu', 'Source Sans Pro', sans-serif;
+  font-family: "Ubuntu", "Source Sans Pro", sans-serif;
 }
 
 .footer a {
   color: #ffffff;
   padding-right: 5px;
 }
-
 </style>
